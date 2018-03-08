e8737fce2507684fc52d3d15be4c4c89a6c10032
PaulineDMR <pauline.desmares@gmail.com>
2018-03-08 21:05:41 +0100

    set index template

---
+++ view/template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title><?= $page_title ?></title>
+	<link rel="stylesheet" type="text/css" href="view/style.css">
+</head>
+
+<body>
+	<?= $page_content ?>
+
+</body>
+</html>

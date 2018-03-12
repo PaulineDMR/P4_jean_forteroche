cb456e716c03214619c12edc15b54cd87195cacb
PaulineDMR <pauline.desmares@gmail.com>
2018-03-12 14:01:55 +0100

    Set the view and correct the link to print a post and his comments in the template.php

--- view/postView.php
+++ view/postView.php
@@ -1,30 +1,27 @@
-<!-- <?php $page_title // = 'Commentaires'; ?>
+<?php $page_title = 'Commentaires'; ?>
 
-<?php // ob_start(); ?>
+<?php ob_start(); ?>
 	
-	<a href="../index.php?action=listPosts">Retour à la liste des articles</a>
+	<a href="index.php?action=listPosts">Retour à la liste des articles</a>
 	<div>
-		<h3><?=  // htmlspecialchars($post["title"]); ?></h3>
-		<p><?= // nl2br(htmlspecialchars($post["content"])); ?></p>
-		<p><?= // htmlspecialchars($post["post_date_fr"]); ?></p>	
+		<h3><?=  htmlspecialchars($post["title"]); ?></h3>
+		<p><?= nl2br(htmlspecialchars($post["content"])); ?></p>
+		<p><?= htmlspecialchars($post["post_date_fr"]); ?></p>	
 	</div>
 
 	<h2>Commentaires</h2>
 
 	<?php
-		//while ($comment = $comments->fetch())
+		while ($comment = $comments->fetch())
 		{
 	?>
-			<h4><strong><?= // htmlspecialchars($comment["author"]); ?></strong> le <?= // htmlspecialchars($comment["comment_date_fr"]); ?></h4>
-			<p><?= // nl2br(htmlspecialchars($comment["comment"])); ?></p>
+			<h4><strong><?= htmlspecialchars($comment["author"]); ?></strong> le <?= htmlspecialchars($comment["comment_date_fr"]); ?></h4>
+			<p><?= nl2br(htmlspecialchars($comment["comment"])); ?></p>
 	<?php		
 		}
-	$comments->// closeCursor();
+	$comments->closeCursor();
 	?>
 
-<?php $page_content // = ob_get_clean(); ?>
+<?php $page_content = ob_get_clean(); ?>
 
-<?php // require('template.php'); ?>
-
-
--->
+<?php require('template.php'); ?>

01f9fb9ebdb27b5a636f95e77dfa83af1faab2d2
PaulineDMR <pauline.desmares@gmail.com>
2018-03-12 13:17:25 +0100

    Create posts_list and post_comments function on the frontend controller

--- controller/frontend.php
+++ controller/frontend.php
@@ -3,19 +3,16 @@
 
 require('model/frontend.php');
 
-/*function posts_list()
-{*/
+function posts_list()
+{
 	$resp = get_posts();
 
 	require('view/indexView.php');
-/*}
+}
 
 function post_comments()
 {
 	$post = get_post($_GET["id"]);
 	$comments = get_comments($_GET["id"]);
 	require('view/postView.php');
-}*/
-
-?>
-
+}

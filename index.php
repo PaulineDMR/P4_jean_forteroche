4246f0b961747dbe432489ab9714ad453b08aa6c
PaulineDMR <pauline.desmares@gmail.com>
2018-03-12 20:23:08 +0100

    Update routeur with new direction when comment added

--- index.php
+++ index.php
@@ -15,6 +15,23 @@ if (isset($_GET["action"])) {
 			echo 'Erreur : aucun identifiant de billet envoyé';
 		}
 	}
+	elseif ($_GET["action"] == "comment")
+	{
+		if (isset($_GET["id"]) && $_GET["id"] > 0)
+		{
+			if (!empty($_POST['pseudo']) && !empty($_POST['comment']))
+			{
+                new_comment($_POST["pseudo"], $_POST["comment"], $_GET["id"]);
+            }
+            else {
+                echo 'Erreur : tous les champs ne sont pas remplis !';
+            }
+		}
+		else
+		{
+			echo 'Erreur : aucun identifiant de billet envoyé';
+		}
+	}
 }
 else {
 	posts_list();

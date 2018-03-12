f07fd3168ab6db8f44bc6bb28f3acd19e52bc17b
PaulineDMR <pauline.desmares@gmail.com>
2018-03-12 11:51:38 +0100

    return to the first version

--- model/frontend.php
+++ model/frontend.php
@@ -1,16 +1,25 @@
 <?php
-// Get posts list from DB p4jf table posts
+// Get info from DB p4jf
 
 function get_posts()
 {
-	$db = dbConnect();
+	try
+	{
+		$db = new PDO('mysql:host=localhost:8889;dbname=p4jf;charset=utf8', 'root', 'root');
+	}
+	catch (Exception $e)
+	{
+	        die('Erreur : ' . $e->getMessage());
+	}
+
+	// $db = dbConnect();
 
 	$resp = $db->query('SELECT id, title, content, DATE_FORMAT(post_date, "%d/%m/%Y à %Hh%imin%ss") AS post_date_fr FROM posts ORDER BY post_date DESC');
 
 	return $resp;
 }
 
-function get_post($post_id)	
+/*function get_post($post_id)	
 {
 	$db = dbConnect();
 	$resp = $db->prepare("SELECT id, title, content, DATE_FORMAT(post_date, '%d/%m/%Y à %Hh%imin%ss') AS post_date_fr FROM posts WHERE id = ?");
@@ -18,6 +27,14 @@ function get_post($post_id)
 
 	$post = $db->fetch();
 	return $post;
+}
+
+function get_comments($post_id)
+{
+	$db = dbConnect();
+	$comments = $db->prepare("SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS comment_date_fr FROM comments WHERE id = ? ORDER BY comment_date DESC");
+	$comments->execute(array($post_id));
+	return $comments;
 }
 
 function dbConnect()
@@ -30,4 +47,8 @@ function dbConnect()
 	{
 	        die('Erreur : ' . $e->getMessage());
 	}
-}
+
+
+}*/
+
+?>

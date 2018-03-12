a1120a09d0fa4de75eabcfcc8f2b8c8e6772d8dc
PaulineDMR <pauline.desmares@gmail.com>
2018-03-12 13:55:32 +0100

    Correct the get_comments function, change the date format and correct the mysql request id -> post_id

--- model/frontend.php
+++ model/frontend.php
@@ -14,16 +14,16 @@ function get_post($post_id)
 {
 	$db = dbConnect();
 	$resp = $db->prepare("SELECT id, title, content, DATE_FORMAT(post_date, '%d/%m/%Y à %Hh%imin%ss') AS post_date_fr FROM posts WHERE id = ?");
-	$db->execute(array($post_id));
+	$resp->execute(array($post_id));
 
-	$post = $db->fetch();
+	$post = $resp->fetch();
 	return $post;
 }
 
 function get_comments($post_id)
 {
 	$db = dbConnect();
-	$comments = $db->prepare("SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS comment_date_fr FROM comments WHERE id = ? ORDER BY comment_date DESC");
+	$comments = $db->prepare("SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%i') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC");
 	$comments->execute(array($post_id));
 	return $comments;
 }

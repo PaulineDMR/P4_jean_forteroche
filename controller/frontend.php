2551a9b7f1182372d6d268a329be7f5a8ff15426
PaulineDMR <pauline.desmares@gmail.com>
2018-03-13 19:30:08 +0100

    Update to POO

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,25 +1,29 @@
 <?php
 // Index frontend controller
 
-require('model/frontend.php');
+require_once('model/PostManager.php');
+require_once('model/CommentManager.php');
 
-function posts_list()
-{
-	$resp = get_posts();
+function posts_list() {
+	$PostManager = new PostManager();
+	$resp = $PostManager->get_posts();
 
-	require('view/indexView.php');
+	require('view/frontend/indexView.php');
 }
 
-function post_comments()
-{
-	$post = get_post($_GET["id"]);
-	$comments = get_comments($_GET["id"]);
-	require('view/postView.php');
+function post_comments() {
+	$postManager = new PostManager();
+	$commentManager = new CommentManager();
+
+	$post = $postManager->get_post($_GET["id"]);
+	$comments = $CommentManager->get_comments($_GET["id"]);
+	require('view/frontend/postView.php');
 }
 
 function new_comment($author, $comment, $post_id)
 {
-	$new_entry = add_comment($author, $comment, $post_id);
+	$commentManager = new CommentManager();
+	$new_entry = $commentManager->add_comment($author, $comment, $post_id);
 	if ($new_entry === false)
 	{
 		throw new Exception('Impossible d\'ajouter le commentaire !');

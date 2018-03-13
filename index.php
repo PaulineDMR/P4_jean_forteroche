2bd44b334c8ee9bab40c54684a5d7d7ca9ca91e2
PaulineDMR <pauline.desmares@gmail.com>
2018-03-13 16:22:49 +0100

    Update the error managment to a 'try/catch' system

--- index.php
+++ index.php
@@ -3,39 +3,42 @@
 require('controller/frontend.php');
 
 // choix de l'affichage selon les donnée de l'url
-if (isset($_GET["action"])) {
-	if ($_GET["action"] == "listPosts") {
+try {
+	if (isset($_GET["action"])) {
+		if ($_GET["action"] == "listPosts") {
+			posts_list();
+		}
+		elseif ($_GET["action"] == "post") {
+			if (isset($_GET["id"]) && $_GET["id"] > 0) {
+				post_comments();
+			}
+			else {
+				throw new Exception('Aucun identifiant de billet envoyé');
+			}
+		}
+		elseif ($_GET["action"] == "comment") {
+			if (isset($_GET["id"]) && $_GET["id"] > 0) {
+				if (!empty($_POST['pseudo']) && !empty($_POST['comment'])) {
+	                new_comment($_POST["pseudo"], $_POST["comment"], $_GET["id"]);
+	            }
+	            else {
+	                throw new Exception('Tous les champs ne sont pas remplis !');
+	            }
+			}
+			else {
+				throw new Exception('Aucun identifiant de billet envoyé');
+			}
+		}
+	}
+	else {
 		posts_list();
 	}
-	elseif ($_GET["action"] == "post") {
-		if (isset($_GET["id"]) && $_GET["id"] > 0) {
-			post_comments();
-		}
-		else {
-			echo 'Erreur : aucun identifiant de billet envoyé';
-		}
-	}
-	elseif ($_GET["action"] == "comment")
-	{
-		if (isset($_GET["id"]) && $_GET["id"] > 0)
-		{
-			if (!empty($_POST['pseudo']) && !empty($_POST['comment']))
-			{
-                new_comment($_POST["pseudo"], $_POST["comment"], $_GET["id"]);
-            }
-            else {
-                echo 'Erreur : tous les champs ne sont pas remplis !';
-            }
-		}
-		else
-		{
-			echo 'Erreur : aucun identifiant de billet envoyé';
-		}
-	}
 }
-else {
-	posts_list();
+catch(Exception $e) {
+	echo 'Erreur: ' . $e->getMessage();
 }
+
+
 
 
 

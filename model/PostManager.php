8b615213d005633933f4ef83aca5e4fa87506bc5
PaulineDMR <pauline.desmares@gmail.com>
2018-03-22 19:29:29 +0100

    Adapt to the POO architecture, creation of the 'CREATE' and 'READ' function

--- model/PostManager.php
+++ model/PostManager.php
@@ -1,19 +1,57 @@
 <?php
 // set PostManager extended class of Manager
-require_once("model/Manager.php");
+require_once("Manager.php");
+require_once("Post.php");
 
 class PostManager extends Manager {
+
+// CREATE a new post in db
+	/**
+	 * @return 
+	 */
+	public function add_new_post ($title, $content) {
+		$db = $this->dbConnect();
+		$insert = $db->prepare("INSERT INTO posts ((id, post_date, title, author, content) VALUES (null, NOW(), ?, null, ?)");
+		$newPost = $insert->execute(array($author, $content));
+
+	    return $newPost;
+	}
+
+
+
+// READ one or all posts from DB
+
+	/**
+	 * @return [type]
+	 */
 	public function get_posts() {
+		$db = $this->dbConnect();
 		$resp = $db->query('SELECT id, title, content, DATE_FORMAT(post_date, "%d/%m/%Y à %Hh%imin%ss") AS post_date_fr FROM posts ORDER BY post_date DESC');
 
-			return $resp;
+		$posts = array();
+
+		while ($donnees = $resp->fetch()) {
+			$post = new Post();
+			$post->hydrate($donnees);
+			$posts[] = $post;
+		}
+		return $posts;
 	}
 
 	public function get_post($post_id) {
+		$db = $this->dbConnect();
 		$resp = $db->prepare("SELECT id, title, content, DATE_FORMAT(post_date, '%d/%m/%Y à %Hh%imin%ss') AS post_date_fr FROM posts WHERE id = ?");
 		$resp->execute(array($post_id));
 
 		$post = $resp->fetch();
 		return $post;
 	}
+
+// UPDATE a post in DB
+
+
+
+// DELETE a post in DB
+
+
 }

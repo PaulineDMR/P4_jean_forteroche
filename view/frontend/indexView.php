7dd3977fd1fe160d9d393cd6a5b45e7e457e3e3c
PaulineDMR <pauline.desmares@gmail.com>
2018-03-22 22:00:11 +0100

    Adapt the view to the new POO architecture

--- view/frontend/indexView.php
+++ view/frontend/indexView.php
@@ -7,46 +7,23 @@
 
 <?php 
 
-/*var_dump($resp);
-echo "<br>";
-echo sizeof($resp);*/
-
-
 for ($index = 0; $index < sizeof($resp); $index++ ) {
 ?>
 
 	<div>
-		<h3><?= htmlspecialchars($resp["$index"]["title"]); ?></h3>
-		<p><?= nl2br(htmlspecialchars($resp["content"])); ?></p>
-		<p><?= htmlspecialchars($resp["post_date_fr"]); ?></p>
-		<a href="index.php?action=post&amp;id=<?= $resp["id"] ?>">Commentaires</a>
+		<h3><?= htmlspecialchars($resp["$index"]->getTitle()); ?></h3>
+		<p><?= nl2br(htmlspecialchars($resp["$index"]->getContent())); ?></p>
+		<p><?= htmlspecialchars($resp["$index"]->getPostDate()); ?></p>
+		<a href="index.php?action=post&amp;id=<?= $resp["$index"]->getId() ?>">Commentaires</a>
 	</div>
 
 <?php
-
 } 
-?>
 
-<!--
-/*foreach ($resp as $key => $value) {
-?>
-	<div>
-		<h3><?php echo htmlspecialchars($resp["title"]); ?></h3>
-		<p><?= nl2br(htmlspecialchars($resp["content"])); ?></p>
-		<p><?= htmlspecialchars($resp["post_date_fr"]); ?></p>
-		<a href="index.php?action=post&amp;id=<?= $resp["id"] ?>">Commentaires</a>
-	</div>
-	
-	-->
-
-<?php
-/*
-
-$resp->closeCursor();
+//$resp->closeCursor();
 
 $page_content = ob_get_clean();
 
 require('template.php');
-*/
 
-?>*/
+?>

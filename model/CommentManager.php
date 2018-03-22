c7ffc0524c09ffc9331c40e783b28d0445f3d656
PaulineDMR <pauline.desmares@gmail.com>
2018-03-22 19:26:48 +0100

    Adapt the code to the POO architecture, creation of the 'CREATE' function

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -1,20 +1,37 @@
 <?php
 // set class CommentManager extended from Manager
-require_once("model/Manager.php");
+require_once("Manager.php");
+require_once("Comment.php");
 
 class CommentManager extends Manager {
 
+// CREATE a new comment in db
+
+	/**
+	 * @param [string]
+	 * @param [string]
+	 * @param [int]
+	 */
+	public function add_comment($author, $comment, $post_id) {
+		$db = $this->dbConnect();
+		$new_comment = $db->prepare("INSERT INTO comments(author, comment, post_id, comment_date) VALUES (?, ?, ?, NOW())");
+		$new_entry = $new_comment->execute(array($author, $comment, $post_id));
+
+	    return $new_entry;
+	}
+
+// READ all comments for one post
+
 	public function get_comments($post_id) {
+		$db = $this->dbConnect();
 		$comments = $db->prepare("SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%i') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC");
 		$comments->execute(array($post_id));
 
 		return $comments;
 	}
 
-	public function add_comment($author, $comment, $post_id) {
-		$new_comment = $db->prepare("INSERT INTO comments(author, comment, post_id, comment_date) VALUES (?, ?, ?, NOW())");
-		$new_entry = $new_comment->execute(array($author, $comment, $post_id));
+// UPDATE comment
 
-	    return $new_entry;
-	}
+// DELETE a comment
+
 }

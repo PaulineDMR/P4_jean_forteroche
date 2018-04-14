96b049c2154b289c1b56d64b24228b8b80cc6530
PaulineDMR <pauline.desmares@gmail.com>
2018-04-14 20:36:01 +0200

    create comments admin view php file

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -23,6 +23,7 @@
 					
 				<p>"C'est avec grand plaisir que je publie en ligne mon nouveau roman "Billet simple pour l'Alaska".</p>
 				<p>Plongez au coeur d'une saga surprenante et décrouvrez chaque semaine un nouvel épisode du livre. Exprimez vous en publiant vos commentaires et partagez avec les autres lecteurs votre ressenti.</p>
+				<img id="photoJean" src="view/frontend/img/man-2785071_640.jpg" alt="portrait d'un homme" name="Photo de Jean Forteroche">
 				<h2>Biographie</h2>
 				<p>Jean Forteroche est né en 1962 à Clermont Ferrand, il etudie le journalisme à Paris et commence à parcourir le continent Américain à l'âge de 22 ans. C'est au cours de ses voyages qu'il écrit ses premières nouvelles puis vient un roman paru en 1989, "Les Portes De L'Oubli". A ce jour, Jean Forteroche a publié 12 romans et continue de partager sa vie entre voyage et écriture.</p>
 			</div>
